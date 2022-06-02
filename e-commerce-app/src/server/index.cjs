bfa14189a62dc8ef707bf1e75651b3c4d0c93e14
Joshua Christensen <joshua@codingwithjosh.com>
2022-06-01 22:21:22 -0500

    I didn't realize the schema needed to be named typeDefs

--- e-commerce-app/src/server/index.cjs
+++ e-commerce-app/src/server/index.cjs
@@ -15,9 +15,9 @@ const { ApolloServer, gql } = require('apollo-server')
 //     stringArray3: [String!]!
 //   }
 // `
-const schema = gql`
+const typeDefs = gql`
   type Query {
-     products: [Product!]!
+    products: [Product!]!
   }
 
   type Product {
@@ -25,7 +25,7 @@ const schema = gql`
     description: String!
     quantity: Int!
     price: Float!
-    onSale: Boolean
+    onSale: Boolean!
   }
 `
 
@@ -43,11 +43,28 @@ const schema = gql`
 //   }
 // }
 const resolvers = {
-  
+  Query: {
+    products: () => [
+      {
+        name: `Trek Marlin 4 Mountain Bike, Matte Trek Black`,
+        description: `Marlin 4 is the perfect gateway to trail riding. It's ideal for new riders who want a mountain bike with knobby tires that can double as a rugged commuter. Disc brakes, front suspension, 21 speeds, and mounts for a rack and fenders make this a great bike for new trail riders and campus commuters alike.`,
+        quantity: 20,
+        price: 619.99,
+        onSale: false
+      },
+      {
+        name: `Trek Procaliber 9.6 Mountain Bike, Blue Carbon Smoke`,
+        description: `Procaliber 9.6 is a race-ready cross country hardtail. Its fast carbon frame boasts a trail-taming IsoSpeed decoupler, quick-rolling 29er wheels, and a RockShox Recon Gold RL suspension fork to keep you fast and smooth over roots and rocks during your most demanding mountain bike rides.`,
+        quantity: 20,
+        price: 2999.99,
+        onSale: false
+      }
+    ]
+  }
 }
 
 const server = new ApolloServer({
-  schema,
+  typeDefs,
   resolvers
 })
 
