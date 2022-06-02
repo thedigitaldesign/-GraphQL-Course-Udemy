8692862b0af644fd71eeccf9076bf7f5f944de8d
Joshua Christensen <joshua@codingwithjosh.com>
2022-06-01 21:32:12 -0500

    Learning wicked cool things, also trying to get TS working

--- e-commerce-app/src/server/index.cjs
+++ e-commerce-app/src/server/index.cjs
@@ -1,21 +1,53 @@
 const { ApolloServer, gql } = require('apollo-server')
 
-const typeDefs = gql`
+//- Types define what the data structure
+//- Scaler types: String, Int, Float, and Boolean
+//- Not null: hello: String!
+// const typeDefs = gql`
+//   type Query {
+//     hello: String! #<-- Not null
+//     world: String
+//     numberOfAnimals: Int
+//     price: Float
+//     isCool: Boolean
+//     stringArray: [String]
+//     stringArray2: [String!]
+//     stringArray3: [String!]!
+//   }
+// `
+const schema = gql`
   type Query {
-    hello: String,
-    world: String
+     products: [Product!]!
+  }
+
+  type Product {
+    name: String!
+    description: String!
+    quantity: Int!
+    price: Float!
+    onSale: Boolean
   }
 `
 
+//- Resolvers return the data defined in the Types
+// const resolvers = {
+//   Query: {
+//     hello: () => 'Hello!',
+//     world: () => 'World!',
+//     numberOfAnimals: () => 2,
+//     price: () => 4.22,
+//     isCool: () => true,
+//     stringArray: () => ['well', 'this', 'is', 'a', null],
+//     stringArray2: () => ['well', 'this', 'is', 'an array'],
+//     stringArray3: () => ['nothing', 'is', 'nullable']
+//   }
+// }
 const resolvers = {
-  Query: {
-    hello: () => "Hello!",
-    world: () => "World!"
-  }
+  
 }
 
 const server = new ApolloServer({
-  typeDefs,
+  schema,
   resolvers
 })
 
