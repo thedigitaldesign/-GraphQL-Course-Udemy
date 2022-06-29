79aadff6c1452aeaefe7b5c2ff4f09627d326520
Joshua Christensen <joshua@codingwithjosh.com>
2022-06-28 21:17:35 -0500

    Pulled the server logic out into it's own root folder

---
+++ 03-blog-app-server/src/index.ts
@@ -0,0 +1,22 @@
+import { ApolloServer, gql } from 'apollo-server'
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`
+const resolvers = {
+  Query: {
+    hello: () => "world"
+  }
+}
+
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.listen().then(({ url }) => {
+  console.log(`Server is ready at: ${url}`)
+})
